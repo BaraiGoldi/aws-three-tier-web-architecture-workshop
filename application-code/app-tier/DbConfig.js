5d8318d7b9d46cfa29e8a53826ccced3a88d73fc
BaraiGoldi <150845723+BaraiGoldi@users.noreply.github.com>
2025-08-26 15:23:38 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.c4xmgs2sap5w.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'S3cur3*Db^Acc3ss99',
+    DB_DATABASE : 'websppdb'
 });
